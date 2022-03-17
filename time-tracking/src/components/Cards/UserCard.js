199bcf498b2652cf5fa4deca3867b4ef0269874e
Guilherme Albano <GuiAlbano10@gmail.com>
2022-03-17 10:41:09 -0300

    feat: responsive grid layout

--- time-tracking/src/components/Cards/UserCard.js
+++ time-tracking/src/components/Cards/UserCard.js
@@ -3,17 +3,35 @@ import styled from 'styled-components'
 import avatar from '../../../src/img/image-jeremy.png'
 
 const CardContainer = styled.div`
-grid-row: 1 / 3;
+@media(max-width: 900px) {
+    grid-area: 1 / 1 / 2 / 2;
+  }
+@media(min-width: 900px) {
+    grid-row: 1 / 3;
+  }
+
 `
 
 const CardHeader = styled.div`
 position: relative;
 background-color: var(--blue);
 border-radius: 15px;
-height: 50vh;
+align-items: center;
 width: 100%;
+padding: 10%;
 margin-bottom: -2vh;
 z-index: 1;
+
+@media(max-width: 900px) {
+    height: 25vh;
+    display: flex;
+    flex-direction: row;
+  }
+
+@media(min-width: 900px) {
+    height: 50vh;
+  }
+
 `
 
 const CardComponent = styled.div`
@@ -22,14 +40,59 @@ border-radius: 0px 0px 15px 15px;
 position: relative;
 margin: 0 auto;
 z-index: -1;
-height: 22vh;
+
 width: 100%;
 padding: 10%;
 cursor: pointer;
 transition: .3s;
+@media(max-width: 900px) {
+    height: 9vh;
+  }
+@media(min-width: 900px) {
+    height: 22vh;
+  }
+
 &:hover {
     transition: .3s;
     background-color: var(--desaturated-blue);
+}
+`
+
+const UserImage = styled.img`
+    width: 70px;
+    height: 70px;
+    margin: 0 8% 8% 0;
+    border: 3px solid #fff;
+    border-radius: 50%;
+
+    @media(max-width: 900px) {
+        margin: 0 5% 1% 0;
+      }
+    
+    @media(min-width: 900px) {
+        margin: 0 8% 8% 0;
+      }
+`
+
+const UserNameContainer = styled.div`
+
+p {
+    font-family: 'Rubik';
+    font-weight: 100;
+    font-size: 15px;
+    @media(max-width: 900px) {
+        margin-bottom: 2%;
+      }
+    
+    @media(min-width: 900px) {
+        margin-bottom: 5%;
+      }
+    
+}
+
+h2 {
+    font-size: 35px;
+    font-weight: 300;
 }
 `
 
@@ -40,9 +103,15 @@ export default function Card( { name }){
     return(
         <CardContainer>
             <CardHeader >
-                <p>Report for</p>
-                <p>{ name }</p>
+
+                <UserImage src={ avatar } />
+                <UserNameContainer>
+                    <p>Report for</p>
+                    <h2>{ name }</h2>
+                </UserNameContainer>
+
             </CardHeader>
+
             <CardComponent>
             </CardComponent>
         </CardContainer>
--- time-tracking/src/components/GridLayout/Grid.js
+++ time-tracking/src/components/GridLayout/Grid.js
@@ -2,13 +2,25 @@ import styled from 'styled-components'
 
 const GridComponent = styled.div`
   display: grid;
-  gap: 8% 2%;  
-  margin: 50vh 20vh 0vh 20vh;
-  transform: translate(0%, -50%);
-
-  @media(min-width: 720px) {
+   
+  
+  @media(min-width: 900px) {
+    gap: 8% 2%; 
     grid-template-columns: 7fr 7fr 7fr 7fr;
+    transform: translate(0%, -50%);
+    margin: 50vh 20vh 0vh 20vh;
   }
+  
+  @media(max-width: 900px) {
+    gap: 4% 2%; 
+    transform: translate(0%, 0%);
+    grid-template-rows: 7fr;
+    margin: 10vh 5vh 10vh 5vh;
+  }
+
+ 
+
+  
   
 `
 
